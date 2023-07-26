39840146a2d3fc64f65db3f0855263622e3dbbd8
delphine abouab <delphine.abouab@outlook.fr>
2023-07-26 15:26:21 +0200

    Basic page and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import './style/index.scss'
 export default function App() {
 
   return (
-    <>
-    </>
+    <div className="board">
+      <h1>Tenzy</h1>
+      <h4>Roll until all dice are the same.<br />Click each die to freeze it at its current value between rolls.</h4>
+      <div className="game-area">
+
+      </div>
+      <button className="btn">Roll</button>
+    </div>
   )
 }
